92c0a954a0f2f3fffdb2289d4a59291ca4e8f569
丛林 <conglin.pro@gmail.com>
2017-06-30 15:08:34 +0800

    更新到数据绑定部分的demo

--- base-build/src/app/component/login.component.ts
+++ base-build/src/app/component/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { environment } from '../../environments/environment';
+
+import { UserModel } from  '../model/UserModel';
 
 
 @Component({
-   templateUrl: '../views/login.component.html',
-  // styleUrls: ['./app.component.css']
+    templateUrl: '../views/login.component.html',
+    styleUrls: ['../../assets/style/login.css'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent {
-  title = 'app works!';
-  environmentName = environment.envName;
+
+    userParams: UserModel = new UserModel();
+
+
+    submit() {
+        console.log(this.userParams)
+    }
 }
